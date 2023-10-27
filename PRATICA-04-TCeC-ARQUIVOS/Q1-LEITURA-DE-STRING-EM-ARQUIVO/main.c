f432e4932b464ca86e6131e90d1c64582d232e73
Felipe Jorge <felipe.jorge.2g@gmail.com>
2023-10-27 08:46:41 -0300

    commit atualizacao de comentarios no código de manipulacao de strings me arquivos

--- PRATICA-04-TCeC-ARQUIVOS/Q1-LEITURA-DE-STRING-EM-ARQUIVO/main.c
+++ PRATICA-04-TCeC-ARQUIVOS/Q1-LEITURA-DE-STRING-EM-ARQUIVO/main.c
@@ -12,7 +12,8 @@ int tamanho_string (char *str){
     }
 }
 
-void inverte_string(char*str, int i, int j){
+void inverte_string(char*str, int i, int j) // i é o primeiro indice da string. j é o ultimo. essses dois parametros delimitam o tamnho da string.
+{
     if(i<j){
         char temp =str[i];
         str[i]= str[j];
@@ -41,7 +42,7 @@ int main()
 
     printf("O tamanho da string foi escrito no arquivo.\n");
 
-    inverte_string(str,0,strlen(str) - 1);
+    inverte_string(str,0,strlen(str) - 1);// a função strlen obtem o tamnho total da string. -1 porque o índice começa em 1.
     fprintf(arq,"A sting inversa é: \"%s\" ", str );
     printf("A string foi invertida e escrita no arquivo.\n");
 
